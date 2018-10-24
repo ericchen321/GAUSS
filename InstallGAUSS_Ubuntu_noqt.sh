aa74be555e90f898a381b139d1775b05ce2e46af
zero-impact <lawsonfulton@gmail.com>
2018-10-24 19:18:55 -0400

    fix spectra

--- InstallGAUSS_Ubuntu_noqt.sh
+++ InstallGAUSS_Ubuntu_noqt.sh
@@ -5,7 +5,7 @@ sudo apt-get install build-essential xorg-dev cmake cmake-data libblas-dev libla
 
 mkdir build
 cd build
-
+ 
 echo "Compile GAUSS"
 cmake .. -DCMAKE_PREFIX_PATH=/opt/Qt5.9.0/5.9/gcc_64/lib/cmake -DEigen3_DIR=/usr/include/eigen3 -DCMAKE_BUILD_TYPE=Release
 make -j 4 all 
